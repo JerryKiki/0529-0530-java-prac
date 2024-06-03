a646ede9957cf2e392067227a1e0633f5ed4d6a5
JerryKiki <gasi758@gmail.com>
2024-06-03 16:50:41 +0900

    0603-3

--- src/main/java/org/example/codeupPrac.java
+++ src/main/java/org/example/codeupPrac.java
@@ -2,19 +2,15 @@ package org.example;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.*;
+
 class codeupPrac {
     public static void main(String[] args) {
         //코드업 문제 푸는 중... 계속 수정됨
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        String[] arr = input.split(" ");
-        int a = Integer.parseInt(arr[0]);
-        int b = Integer.parseInt(arr[1]);
-        if (a == 1 && b == 1){
-            System.out.println(1);
-        }
-        else {
-            System.out.println(0);
-        }
+        int input = sc.nextInt();
+        int time = input/60;
+        int sec = input%60;
+        System.out.printf("%d %d\n", time, sec);
     }
 }
