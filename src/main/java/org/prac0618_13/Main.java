76399812e90dbc371ce27280fbe9ba6715909f68
JerryKiki <gasi758@gmail.com>
2024-06-19 14:49:40 +0900

    0619-2

--- src/main/java/org/prac0618_13/Main.java
+++ src/main/java/org/prac0618_13/Main.java
@@ -40,13 +40,30 @@ class 전사 {
     void 자기소개() {
         System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
     }
+
     void 공격() {
         this.a무기.attack(this.이름);
     }
 }
 
-class 무기 {
-    public void attack(String userName) {}
+abstract class 무기 {
+    //추상메서드를 가지고 있으면 추상 클래스여야함 (이때는 하나 추상, 하나 구상이라 반쪽짜리 추상클래스)
+    abstract void attack(String userName); // 추상메서드 : 이때는 오버라이드 필수
+
+    void 방어() {
+    } // 구상메서드 : 이때는 오버라이드 선택
+}
+
+interface 예시 {
+    //아예 추상메서드만 가지고 있다면, abstract class 부분을 interface라고 써줄 수 있다
+    //interface : 이 안에 들어있는 건 모두 추상메서드라는 것을 알려준다
+
+    void 작동(String 공격자이름);
+    //void 방어() {}; : 이 안에 바디를 구현하려고 하면 오류뜬다.
+}
+
+interface 예시2 {
+    void 방어();
 }
 
 class 칼 extends 무기 {
@@ -58,6 +75,8 @@ class 칼 extends 무기 {
 class 활 extends 무기 {
     public void attack(String userName) {
         System.out.println(userName + "(이)가 활로 공격합니다.");
-    };
+    }
+
+    ;
 }
 
--- src/main/java/org/prac0618_14_hw/Main.java
+++ src/main/java/org/prac0618_14_hw/Main.java
@@ -39,42 +39,35 @@ class 전사 {
     }
 
     void 공격() {
-        String 공격방식 = this.a무기.attack();
-        System.out.println(this.이름 + "이(가) " + 공격방식);
+        String 무기이름 = this.a무기.weaponName();
+        System.out.println("공격1: " + this.이름 + "(이)가 " + 무기이름 + "(으)로 공격합니다.");
+    }
+
+    //더 간편한 방식
+    void 공격2() {
+        this.a무기.공격자명 = this.이름;
+        this.a무기.공격2();
     }
 }
 
 abstract class 무기 {
-    //추상메서드를 가지고 있으면 추상 클래스여야함 (이때는 하나 추상, 하나 구상이라 반쪽짜리 추상클래스)
-    abstract String attack(); // 추상메서드 : 이때는 오버라이드 필수
-    void 방어() {} // 구상메서드 : 이때는 오버라이드 선택
-}
-
-interface 예시 {
-    //아예 추상메서드만 가지고 있다면, abstract class 부분을 interface라고 써줄 수 있다
-    //interface : 이 안에 들어있는 건 모두 추상메서드라는 것을 알려준다
-
-    void 작동(String 공격자이름);
-    //void 방어() {}; : 이 안에 바디를 구현하려고 하면 오류뜬다.
-}
-
-interface 예시2 {
-    void 방어();
-}
-
-class example implements 예시, 예시2 { //implements는 여러개 상속 받을 수 있다. 어차피 전부 오버라이드 할 것이라 헷갈리지 않기 때문
-    public void 작동(String 공격자이름) {//인터페이스한테서 받아오면 public 해줘야됨
-        System.out.println(공격자이름 + "예시");
-    }
-    public void 방어() {
-        System.out.println("implements 상속 예시");
-    }
+    String 공격자명; //새로 배운 방식에 필요한 요소
+    abstract String weaponName();
+    abstract void 공격2();
 }
 
 class 칼 extends 무기 {
-    String attack() { return "칼로 공격합니다."; }
+    String weaponName() { return "칼"; }
+
+    void 공격2() {
+        System.out.println("공격2: " + 공격자명 + "(이)가 칼(으)로 공격합니다.");
+    }
 }
 
 class 활 extends 무기 {
-    String attack() { return "활로 공격합니다."; }
+   String weaponName() { return "활"; }
+
+    void 공격2() {
+        System.out.println("공격2: " + 공격자명 + "(이)가 활(으)로 공격합니다.");
+    }
 }
